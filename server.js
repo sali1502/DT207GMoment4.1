491e28ac312ea9dbb39424bb68e05c031ed1b08f
sali1502 <sali1502@student.miun.se>
2024-07-21 21:18:06 +0200

    Cors

--- server.js
+++ server.js
@@ -4,12 +4,16 @@
 
 const express = require("express");
 const bodyParser = require("body-parser");
+const cors = require("cors");
 const authRoutes = require("./routes/authRoutes");
 require("dotenv").config();
 
+// Initiera Express
 const app = express();
 const port = process.env.PORT || 3000;
+
 app.use(bodyParser.json());
+app.use(cors());
 
 // Routes
 app.use("/api", authRoutes);
